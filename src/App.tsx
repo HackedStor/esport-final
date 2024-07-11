c0ff32365abd2a8c4201d111e0db8bf3c0323baf
HackedStor <element.fact9@gmail.com>
2024-07-11 21:35:09 +0200

    feat: Add shadcn
    Add lib shadcn
    
     Add charts, select and others components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { Button } from './components/ui/button'
+import { Button } from './components/ui/button';
+import { Component } from './myComponent';
+
+
 
 function App() {
 
   return (
     <>
-      <Button>Click me</Button>
+      <div className='bg-background h-screen flex flex-col items-center gap-52'>
+        <Button variant="default" className='w-min'>Click me</Button>
+        <Button variant="outline" className='w-min'>Button</Button>
+        <div className="w-1/2">
+          <Component />
+        </div>
+      </div>
     </>
   )
 }
