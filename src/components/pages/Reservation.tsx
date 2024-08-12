261ce0710f3defa3680b8f1d3573c1d4f628c8ca
LeVraiArdox <ardox@axos-project.com>
2024-08-12 12:22:44 +0200

    Reservation responsive

--- src/components/pages/Reservation.tsx
+++ src/components/pages/Reservation.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import FormReservation from '../perso/FormReservation';
 import TitleInscription from '../perso/TitleInscription';
+import "../../assets/css/reservation.css"
 
 const Reservation: React.FC = () => {
   return (
-    <div className="cus_container">
+    <div className="cus_container_res">
         <TitleInscription />
         <FormReservation />
     </div>
