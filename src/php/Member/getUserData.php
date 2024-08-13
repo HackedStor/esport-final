372701f13fc8753639fce7c5d53585709391173c
HackedStor <element.fact9@gmail.com>
2024-08-13 16:23:57 +0200

    getUserData CORS fix

--- src/php/Member/getUserData.php
+++ src/php/Member/getUserData.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
